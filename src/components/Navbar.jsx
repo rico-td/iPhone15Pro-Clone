6181f684a32cc48fda758ba0499e74b77dc56cab
Rico.TechDev <151479600+rico-td@users.noreply.github.com>
2024-07-10 00:02:27 +0200

    fixing render issue

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,16 @@
-import React from 'react'
 import { appleImg, searchImg, bagImg } from '../utils'
 import { navLists } from '../constants'
 
 const Navbar = () => {
   return (
+
     <header className='w-full py-5 sm:px-10 px-5 flex justify-between items-center'>
-      <nav className='flex w-ful screen-max-width'> 
+      <nav className='flex w-full screen-max-width'>
         <img src={appleImg} alt='Apple' width={14} height={18} />
         
         <div className='flex flex-1 justify-center max-sm:hidden'>
             {navLists.map((nav) => ( 
-                <div key={nav} className='px-5 text-sm cursor-pointer text-gray hover:text-white transition-all'>
+                <div key={nav} className="px-5 text-sm cursor-pointer text-gray hover:text-white transition-all">
                     {nav}   
                 </div>
             ))}
@@ -19,7 +19,6 @@ const Navbar = () => {
             <img src={searchImg} alt='search' width={18} height={18}/> 
             <img src={bagImg} alt='bag' width={18} height={18}/>
         </div>
-
       </nav>
     </header>
   )
